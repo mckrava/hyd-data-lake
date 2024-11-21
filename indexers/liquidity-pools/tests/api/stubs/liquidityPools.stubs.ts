04820b3b9b68e0825c023fd503342bf7f584bbe1
mckrava <mckrava2@gmail.com>
2024-11-21 11:29:25 -0500

    9

--- indexers/liquidity-pools/tests/api/stubs/liquidityPools.stubs.ts
+++ indexers/liquidity-pools/tests/api/stubs/liquidityPools.stubs.ts
@@ -16,10 +16,14 @@ export default {
         hash: '0xfa8061028cdc118a785df37876163229da3c7ff6db586e16d3fce0d37548aa61',
       },
       blockB: {
+        height: 3684428,
+        hash: '0x895e0a8c8db74e70a127f280e71ab781ca7a14dfb34c9fa9d5e8aea34d025332',
+      },
+      blockC: {
         height: 3706163,
         hash: '0xa275622c9b96acffdf1e90d0878453114c30b67d7b0656a34c186e3245d4fc13',
       },
-      blockC: {
+      blockD: {
         height: 4198163,
         hash: '0xa63aae8efb060fd5d6fc0aadeebef3c814465a8ea52ced668b2fcfdef977f70c',
       },
